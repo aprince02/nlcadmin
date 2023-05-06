cdca229f5c5a1dca525113021c5df83f44b2b778
aprince02 <aprince02@icloud.com>
2023-05-06 12:17:39 +0100

    Update ProBooks Accounting Push.bat

--- ProBooks Accounting Push.bat
+++ ProBooks Accounting Push.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 set "commit_msg=Automatic commit on %date% at %time% by %username%"
-echo Pushing to GitHub...
+echo Pushing to GitHub....
 git add .
 git commit -m "!commit_msg!"
 git push origin dev
